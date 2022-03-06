5e7cae874f0cdd98aabe3b865505bed7a5069463
Narender Singh bisht <89627914+narendercoder@users.noreply.github.com>
2022-03-06 11:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,21 @@ let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
-
-let playpause_btn = document.querySelector('.playpause-track');
-let next_btn = document.querySelector('.next-track');
-let prev_btn = document.querySelector('.prev-track');
-
-let seek_slider = document.querySelector('.seek_slider');
-let volume_slider = document.querySelector('.volume_slider');
+let img = document.querySelector('.image');
+let playpause_btn = document.getElementById('playpause-track');
+let next_btn = document.getElementById('next-track');
+let prev_btn = document.getElementById('prev-track');
+let repeat_btn = document.getElementById('repeat-track');
+let progress = document.getElementById('progress');
+let progress_div = document.getElementById('progress_div');
 let curr_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
-let randomIcon = document.querySelector('.fa-random');
-let curr_track = document.createElement('audio');
+let random_btn = document.getElementById('random-track');
+let music =document.querySelector('audio');
 
 let track_index = 0;
-let isPlaying = false;
 let isRandom = false;
-let updateTimer;
-
+let isPlaying = false;
 const music_list = [
   
     {
@@ -56,102 +54,143 @@ const music_list = [
 loadTrack(track_index);
 
 function loadTrack(track_index){
-    clearInterval(updateTimer);
     reset();
-
-    curr_track.src = music_list[track_index].music;
-    curr_track.load();
-
+    music.src = music_list[track_index].music;
+    music.load();
     track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
     now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
 
-    updateTimer = setInterval(setUpdate, 1000);
-
-    curr_track.addEventListener('ended', nextTrack);
 }
 
+random_btn.addEventListener("click",() =>{
+    isRandom ? pauseRandom() : playRandom();
+});
+function playRandom(){
+    isRandom = true;
+    random_btn.classList.add('randomActive');
+}
+function pauseRandom(){
+    isRandom = false;
+    random_btn.classList.remove('randomActive');
+}
+
+
+// *play music
+const playMusic = ()=>{
+    isPlaying = true;
+    music.play();
+    playpause_btn.classList.replace("fa-play-circle", "fa-pause-circle");
+    track_art.classList.add('rotate');
+};
+
+// *pause music
+const pauseMusic = ()=>{
+    isPlaying = false;
+    music.pause();
+    playpause_btn.classList.replace("fa-pause-circle", "fa-play-circle");
+    track_art.classList.remove('rotate');
+};
+
+
+playpause_btn.addEventListener("click",()=>{
+    // if(isPlaying){
+    //     pauseMusic();
+    // }else{
+    //     playMusic()
+    // }
+    isPlaying ? pauseMusic() : playMusic();
+});
 
 function reset(){
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
-    seek_slider.value = 0;
+
 }
-function randomTrack(){
-    isRandom ? pauseRandom() : playRandom();
-}
-function playRandom(){
-    isRandom = true;
-    randomIcon.classList.add('randomActive');
-}
-function pauseRandom(){
-    isRandom = false;
-    randomIcon.classList.remove('randomActive');
-}
-function repeatTrack(){
-    let current_index = track_index;
-    loadTrack(current_index);
-    playTrack();
-}
-function playpauseTrack(){
-    isPlaying ? pauseTrack() : playTrack();
-}
-function playTrack(){
-    curr_track.play();
-    isPlaying = true;
-    track_art.classList.add('rotate');
-    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
-}
-function pauseTrack(){
-    curr_track.pause();
-    isPlaying = false;
-    track_art.classList.remove('rotate');
-    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-}
-function nextTrack(){
-    if(track_index < music_list.length - 1 && isRandom === false){
-        track_index += 1;
-    }else if(track_index < music_list.length - 1 && isRandom === true){
+// loadTrack(music_list[1]);
+
+const next_track = () =>{
+    //play random
+    if(isRandom === true){
         let random_index = Number.parseInt(Math.random() * music_list.length);
         track_index = random_index;
-    }else{
-        track_index = 0;
     }
+    //play next
+   else if(isRandom === false){
+    track_index = (track_index + 1 ) % music_list.length;
+}
+else{
+    track_index =0;
+}
     loadTrack(track_index);
-    playTrack();
+    playMusic();
 }
-function prevTrack(){
-    if(track_index > 0){
-        track_index -= 1;
-    }else{
-        track_index = music_list.length -1;
-    }
+const prev_track = () =>{
+    track_index = (track_index - 1  + music_list.length) % music_list.length;
     loadTrack(track_index);
-    playTrack();
-}
-function seekTo(){
-    let seekto = curr_track.duration * (seek_slider.value / 100);
-    curr_track.currentTime = seekto;
-}
+    playMusic();
 
-function setUpdate(){
-    let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
-        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
-        seek_slider.value = seekPosition;
+ }
+next_btn.addEventListener('click', next_track);
+prev_btn.addEventListener('click', prev_track);
 
-        let currentMinutes = Math.floor(curr_track.currentTime / 60);
-        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
-        let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
-        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
-        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+//update music current time and duration
+music.addEventListener("timeupdate",(event)=>{
 
-        curr_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationMinutes;
+    const {currentTime, duration} = event.srcElement;
+
+    let progress_time = (currentTime/duration)*100;
+    progress.style.width = `${progress_time}%`;
+   
+    // duration time update 
+    let min_duration = Math.floor(duration / 60);
+    let sec_duration = Math.floor(duration % 60);
+    
+    if(sec_duration < 10){
+        sec_duration = `0${sec_duration}`;
     }
-}
+    if(min_duration < 10){
+
+        min_duration = `0${min_duration}`;
+    }
+    if(duration){
+        total_duration.textContent = `${min_duration}:${sec_duration}`;
+    }
+    
+    // current time update 
+    let min_currentTime = Math.floor(currentTime / 60);
+    let sec_currentTime = Math.floor(currentTime % 60);
+    
+    if(sec_currentTime < 10){
+
+        sec_currentTime = `0${sec_currentTime}`;
+    }
+    if(min_currentTime < 10){
+
+        min_currentTime = `0${min_currentTime}`;
+    }
+    curr_time.textContent = `${min_currentTime}:${sec_currentTime}`;
+
+});
+
+//automatic play next music  when prev music end.
+music.addEventListener('ended',next_track);
+
+//progress onclick functionality
+
+progress_div.addEventListener("click",(event) =>{
+   let progressWidth = progress_div.clientWidth;
+   let clickedOffsetX = event.offsetX
+   const songduration = music.duration; 
+   music.currentTime  = (clickedOffsetX/ progressWidth)*songduration;
+   playMusic();
+});
+
+//repeat music
+repeat_btn.addEventListener("click",(event) =>{
+    let curent_index = track_index;
+    loadTrack(curent_index);
+    playMusic();
+ });
